d394e2492b58d7fe0e62eb9aa95cae77509b2070
Chih-Yu Chen <cy.chen@roometric.com>
2025-12-22 10:01:28 +0100

    do not move largest sfm model out

--- hloc/reconstruction.py
+++ hloc/reconstruction.py
@@ -125,17 +125,7 @@ def run_reconstruction(
     logger.info(
         f"Largest model is #{largest_index} " f"with {largest_num_images} images."
     )
-
-    for filename in [
-        "images.bin",
-        "cameras.bin",
-        "points3D.bin",
-        "frames.bin",
-        "rigs.bin",
-    ]:
-        if (sfm_dir / filename).exists():
-            (sfm_dir / filename).unlink()
-        shutil.move(str(models_path / str(largest_index) / filename), str(sfm_dir))
+    
     return reconstructions[largest_index]
 
 
